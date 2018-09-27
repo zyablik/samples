7753b09e793ebe7adcdea3658e9cde7a3cbe476e
Alexey Kulish <alex.kulish@collabio.team>
2018-09-27 17:49:09 +0300

    bios_vbe_fb_kernel: pci enumeration
    https://habr.com/company/neobit/blog/176707/

--- bios_vbe_fb_kernel/include/types.h
+++ bios_vbe_fb_kernel/include/types.h
@@ -3,5 +3,7 @@
 
 #define NULL 0
 typedef unsigned int size_t;
-
+typedef unsigned long u32;
+typedef unsigned short u16;
+typedef unsigned char u8;
 #endif
--- bios_vbe_fb_kernel/kernel.c
+++ bios_vbe_fb_kernel/kernel.c
@@ -1,9 +1,11 @@
 #include "printf.h"
 #include "screen.h"
 #include "types.h"
+#include "pci.h"
 
 void main(void)
 {   
     clear_screen();
     printf("\n>>> Hello World!\n");
+    PCIScan();
 }
